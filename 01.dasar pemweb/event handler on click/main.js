fe244d8e4c16d9a8acd57c6d383562d29dc7f2cf
KaisarF <90669229+KaisarF@users.noreply.github.com>
2022-09-12 15:05:07 +0700

    Add files via upload

---
+++ 01.dasar pemweb/event handler on click/main.js
@@ -0,0 +1,28 @@
+// const tamu = prompt('Siapakah Anda?');
+// alert('Selamat datang ' + tamu + '!');
+
+
+
+function welcome() {
+    alert('Sim salabim muncullah elemen-elemen HTML!');
+    const contents = document.querySelector('.contents');
+    contents.removeAttribute('hidden');
+
+}
+
+function increment() {
+    document.getElementById('count').innerText++;
+
+    if (document.getElementById('count').innerText == 7) {
+        const hiddenMessage = document.createElement('h4');
+        hiddenMessage.innerText = 'Selamat! Anda menemukan hadiah tersembunyi...';
+        const image = document.createElement('img');
+        image.setAttribute('src', 'https://i.ibb.co/0V49VRZ/catto.jpg');
+        const contents = document.querySelector('.contents');
+        contents.appendChild(hiddenMessage).appendChild(image);
+    }
+}
+
+    document.getElementById('incrementButton').onclick = increment;
+
+    
